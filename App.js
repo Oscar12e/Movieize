e44109fc6e04e38008ddd6bd10f2cd56bc257efa
Oscar <oscarcortesc12@gmail.com>
2018-06-16 01:26:21 -0600

    Creada pagina web
    Se tiene la pagina web disponible con los widgets para ingresar los parametros de busqueda.
    
    Esta pagina es cargada dentro del servidor.
    Dicha pagina, carga el canvas donde se mostrará el grafico que el usuario desee ver.

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ var cors = require('cors') // para soportar cross domain
 var express = require('express'); // servidor web
 var bodyParser = require('body-parser'); // para recibir y parsear content en formato json
 
+
 // constante para definir el puerto a ser usado
 var PORT_NUMBER = 8000;
 
 // se inicia el servidor web express
-var app = express()
+var app = new express()
 
 // iniciar el parsing de json
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,30 +16,20 @@ app.use(bodyParser.json());
 // para habilitar cross domain
 app.use(cors())
 
-// publicar contenido estatico que esta en ese folder
-//app.use(express.static("C:\\Users\\rodri\\OneDrive\\Personales\\Itcr\\SemI2018\\Analisis de Algoritmos\\node"));
-app.use(express.static("C:\\MovieStaticData"));
+// publicar contenido estatico que esta en ese folder -- en la raiz
+app.use(express.static(__dirname));
 
-
-app.use((req, res, next)=>{
-    const error = new Error('Not found');
-    error.status(404);
-    next(error);
-});
-
-
-app.use((error, req, res, next)=>{
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+app.get('/home', function(request, response){
+    response.sendfile('index.html');
 });
 
 
 app.get('/getchart', function(req, res) {
 	res.send(req.query.temp+' '+ req.query.title+ ' --- Hola mundo');
+});
+
+app.get('/movieQuery', function(req, res) {
+	res.send(req.query.temp + ' -- Im working');
 });
 
 
@@ -51,3 +42,5 @@ app.post('/savechart', function(req, res) {
 // escuchar comunicacion sobre el puerto indicado en HTTP
 app.listen(PORT_NUMBER);
 console.log("Listening on port "+PORT_NUMBER)
+
+
--- sketch.js
+++ sketch.js
@@ -1,44 +1,40 @@
 let ball;
 
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	loadJSON("data.json", drawData);
+}
+
+function drawData(data){
+	
 }
 
 
 // Runs repeatedly until exit() is called.
 function draw() { 
   background(100);
-  ball = new Ball(mouseX, mouseY);
+  ball = new Bubble(mouseX, mouseY, 35);
   ball.show();
-}
-
-// Runs when mouse is clicked.
-function mouseClicked() {
-	background(500);
-	ball.show();
 }
 
 
 // Class and constructor for the ball.
 
-class Ball {
+class Bubble {
 
-  constructor (initX, initY) {
-    this.x = initX;
-    this.y = initY;
+  constructor (pX, pY, pSize) {
+    this.x = pX;
+    this.y = pY;
+	this.size = pSize;
   }
   
   show() {
 	stroke(255);
 	strokeWeight(4);
-	noFill();
-    ellipse(this.x, this.y, 25, 25);
+    ellipse(this.x, this.y, this.size, this.size);
   }
 }
-  /*function update() {
-    
-    
-  }
 
-  */
+  
 //}
