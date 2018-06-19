3bb91d07c81ef7438fef2104206a742d83f6211e
Óscar Cortés Cordero <oscarcortesc12@gmail.com>
2018-06-19 01:15:38 -0600

    Busqueda < O(n)
    Se agregan las funciones encargadas de hacer la busqueda y obtener los datos de lass peliculas en un tiempo teorico de O(c).
    
    Además de detecta la que la comunicación que se usa no es la que se pide en el documento, por lo que se procede a cambiarla.

--- App.js
+++ App.js
@@ -3,13 +3,11 @@
 
 
 //Imports
-var cors = require('cors') // para soportar cross domain
+var cors = require('cors')        // para soportar cross domain
 var express = require('express'); // servidor web
 var bodyParser = require('body-parser'); // para recibir y parsear content en formato json
-var fs = require('fs'); //Used to read local readFileSync
-var socket = require("socket.io");
-
-console.log("App server is running.");
+var fs = require('fs');             //Used to read local readFileSync
+var socket = require("socket.io"); //Used to alow communication
 
 
 //Constant values
@@ -17,11 +15,171 @@ var PORT_NUMBER = 8000; // constante para definir el puerto a ser usado
 
 //Variables for the server
 var app = new express();
-var movieData = JSON.parse(fs.readFileSync("data/movies.json", "utf8"));
-console.log(movieData[0]["year"]);
-var io;
+var movieJsonList = JSON.parse(fs.readFileSync("data/movies.json", "utf8"));
+var io;   //Variable para los sockets
+//Movie data holds as a dict
 
-//Sockes
+var movieData = {
+  "title": {},
+  "year" : {},
+  "director": {},
+  "cast":  {},
+  "genre": {}
+};
+
+var colorGender = {
+	"Comedy": (0,255,0),
+	"null": (255,255,255)
+}
+
+
+/**/
+
+/*
+|Key| -> values
+|Word| -> Index
+*/
+function storeMovieDataInHash(pCategory, pData){
+  var data = String(pData[pCategory]).toLowerCase(); //Se pasa a string para luego poder aplixar toLowerCase y
+  for (var substring of data.split(' ')){
+    if ( !(substring in movieData[pCategory]) )
+      movieData[pCategory][substring] = [];
+    movieData[pCategory][substring].push(movieIndex);
+  }
+}
+
+function loadMovieDataInHash(pCategory, pSearch){
+  var search = String(pSearch[pCategory]).toLowerCase(); //Se pasa a string para luego poder aplixar toLowerCase y
+  var coincidencesFound = [];
+
+  for (var substring of search.split(' ')){
+    if ( (substring in movieData[pCategory]) ) //Si se encuentra el substring dentro de la categoria, se
+      coincidencesFound.push(movieData[pCategory][substring]);
+  }
+
+  return coincidencesFound;
+}
+
+function loadMovieDataInHash_Year(pSearch){
+  var searchBegin = pSearch["yearBegin"];
+  var searchEnd = pSearch["yearEnd"];
+  var coincidencesFound = [];
+
+  if (!((searchBegin == NaN || searchBegin == null) && (searchEnd == NaN || searchEnd == null)))
+    for (var currentYear = searchBegin; currentYear <= searchEnd; currentYear++){
+      var currentKey = String(currentYear);
+      if ( (currentKey in movieData["year"]) ) //Si se encuentra el substring dentro de la categoria, se
+        coincidencesFound.push(movieData["year"][currentKey]);
+    }
+
+  return coincidencesFound;
+}
+
+
+
+//Dinosaurio que permitira que la busqueda de datos sea nucho más llevadera
+/**
+  Se encarga de leer el JSON para poder hacer busquedas más facilmente.
+  Se usa movieIndex para contar cada una de las peliculas, ya que solo de esta
+  di para obtener el index de cada pelicula.
+*/
+var movieIndex = 0;
+function parseMovieData(pMovie){
+  storeMovieDataInHash("title", pMovie);
+  storeMovieDataInHash("year", pMovie);
+  storeMovieDataInHash("director", pMovie);
+  storeMovieDataInHash("cast", pMovie);
+  storeMovieDataInHash("genre", pMovie);
+
+  if (movieIndex < 5){
+    console.log(String(pMovie["title"]));
+    console.log(String(pMovie["year"]));
+    pMovie["director"];
+    pMovie["cast"];
+    pMovie["genre"];
+    console.log(movieData);
+  }
+
+  movieIndex++;
+}
+
+/*
+*/
+function shortestListIndexInMatrix(pMatrix){
+  console.log(pMatrix.length);
+  var shortesListIndex = 0;
+
+  for (var listIndex = 1;  listIndex < pMatrix.length; listIndex++){
+    if (pMatrix[listIndex].length < pMatrix[shortesListIndex].length)
+      shortesListIndex = listIndex;
+  }
+
+  return shortesListIndex;
+}
+
+
+/*Naive function that counts how many times an element in a list appaer on the
+whole matrix
+*/
+function listElementsOcurrenceInMatrix(pMatrix, pListIndex){ //Dinosaur preprocess
+  var occurencesHash = {};
+  var shortesList = pMatrix[pListIndex];
+  var elementOccurrences = 0;
+
+  for (var elementIndex = 0; elementIndex < shortesList.length; elementIndex++){
+    var element =  shortesList[elementIndex];
+    //var key = String(element);
+
+    // = 1; //Inicia en uno pues esta en la lista que toma
+    elementOccurrences = 1;
+    for(var listIndex = 0; listIndex < pMatrix.length; listIndex++){
+
+
+      if (listIndex == pListIndex)
+        continue;
+      if (binarySearch(pMatrix[listIndex], element) != -1)
+        elementOccurrences++;
+    }
+
+    var key = String(elementOccurrences);
+
+    if (!(key in occurencesHash))
+      occurencesHash[key] = [];
+
+    occurencesHash[key].push(element);
+
+  }
+  return occurencesHash;
+
+}
+
+
+function binarySearch(pList, pValue) {
+    var half = Math.floor(pList.length / 2);
+
+    if (pList[half] === pValue) {
+        return pList[half];
+    } else if (pList[half] < pValue && pList.length > 1) {
+        return binarySearch(pList.slice(half, Number.MAX_VALUE), pValue);
+    } else if (pList[half] > pValue && pList.length > 1) {
+        return binarySearch(pList.slice(0, half), pValue);
+    } else {
+
+        return -1;
+    }
+}
+
+/*
+  Function returns the element that has 'intersecAmount' intersections in the matrix.
+*/
+function getElementIntersecInList(intersectHash, intersecAmount){
+  return intersectHash[intersecAmount];
+}
+
+//Final de las funciones
+
+movieJsonList.forEach(parseMovieData); //Se filtran los datos para obtener quienes
+
 
 // iniciar el parsing de json
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,26 +195,16 @@ app.get('/home', function(request, response){
     response.sendfile('index.html');
 });
 
-app.get('/getmoviedata', function(req, res){
-    res.send(movieData);
-});
-
-app.get('/getMoviesData', function(req, res){
-	//var moviesResult = [];
-	res.send(movieData);
-});
-
-
+/* Codigo de la plantila que no se usa en ese momento
 app.get('/getchart', function(req, res) {
 	res.send(req.query.temp+' '+ req.query.title+ ' --- Hola mundo');
 });
-
-
 
 app.post('/savechart', function(req, res) {
 	console.log(req.body);
 	res.send(req.body.words+ ' --- Hola mundo2');
 });
+*/
 
 
 // escuchar comunicacion sobre el puerto indicado en HTTP
@@ -65,10 +213,34 @@ io = socket(server); //Starts the comunication between server and clients
 
 io.sockets.on('connection', newConnection);
 
+
 function newConnection(pSocket){
   console.log("New conection registered: " + pSocket.id);
+
+  pSocket.on('searchMovies', getMoviesData);
+
+  /* Tres funciones para obtner los datos de filtrado */
+  function getMoviesData (data){      //se recibe la información y se pasa como parametro a la funcion
+    var searchResults = [];           //A matrix that stores all the results found
+
+    searchResults = loadMovieDataInHash("title", data).concat(loadMovieDataInHash_Year(data),
+      loadMovieDataInHash("director", data), loadMovieDataInHash("cast", data), loadMovieDataInHash("genre", data));
+
+    if (searchResults.length == 0){
+      return;
+    }
+
+    var criteriaAmount =  String(searchResults.length);
+
+    var occurencesHash = listElementsOcurrenceInMatrix(searchResults, shortestListIndexInMatrix(searchResults));
+    var listIntersectResults = getElementIntersecInList(occurencesHash, criteriaAmount);
+    console.log(listIntersectResults);
+    pSocket.emit('drawBubbles', listIntersectResults);
+  }
 
 }
 
 
 console.log("Listening on port "+PORT_NUMBER)
+console.log("App server is running.");
+console.log(movieJsonList[18637])
--- sketch.js
+++ sketch.js
@@ -1,36 +1,30 @@
 //Presentation layer - Page
-var socket;
+
 let ball; 			//Ball create to de-stress myself
 var moviesData;		//Created to stored the json
 var showingData; 	//JSON for currently shown data
 
 //UI its a dictonary that holds all the acces to the component to the user interface interaction
+var socket;
 var ui = {
-	"TextBox": {}
+	"TextBox": {},
+	"Button" : {}
 };
 
 //Components use to get the parameters from users
 var title, genre, actors, director, yearBegin, yearEnd, buttonSearch;
 
 //Codigos de colores para cada una de las burbujas según su genero
-const COLOR_GENDER = {
-	"Comedy": (0,255,0),
-	"null": (255,255,255)
-}
 
 function preload() {
     var url = 'https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json';
-    //moviesData = loadJSON(url);
-		//loadBytes("https://localhost:8000/getchart", loadMovies);
-
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	socket = io.connect('http://localhost:8000');
-
+	socket.on('drawBubbles', drawBubbles);
 	loadComponents();
-	showingData = [];
 }
 
 
@@ -38,39 +32,36 @@ function loadMovies(data){
 	//moviesData = bin25String(data["bytes"]);
 }
 
+//Se puede hacer MUCHO mejor, lo sé, pero el tiempo..
 function loadComponents(){
 	createElement('p', 'Titulo:').position(20,65);
-	title = createInput();
-	title.position(80, 80);
+	ui.TextBox["title"] =  createInput().position(80, 80);
 
 	createElement('p', 'Genero:').position(270,65);
-	genre = createInput();
-	genre.position(330, 80);
+	ui.TextBox["genre"] =  createInput().position(330, 80);
+
 
 	createElement('p', 'Actores:').position(520,65);
-	actors = createInput();
-	actors.position(580, 80);
+	ui.TextBox["cast"] =  createInput().position(580, 80);
 
 	createElement('p', 'Director:').position(770, 65);
-	director = createInput();
-	director.position(830, 80);
+	ui.TextBox["director"] =  createInput().position(830, 80);
+
 
 	createElement('p', 'Rango de años:').position(20,95);
 
 	createElement('p', 'Inicio ').position(150,95);
-	yearBegin = createInput();
-	yearBegin.position(200, 110);
+	ui.TextBox["yearBegin"] =  createInput().position(200, 110);
+
 
 	createElement('p', 'Fin ').position(400,95);
-	yearEnd = createInput();
-	yearEnd.position(450, 110);
+	ui.TextBox["yearEnd"] =  createInput().position(450, 110);
 
-	buttonSearch = createButton('Consultar');
-	buttonSearch.position(620, 110);
-	buttonSearch.mousePressed(searchData);
+
+	ui.Button["buttonSearch"] = createButton('Consultar').position(620, 110);
+	ui.Button["buttonSearch"].mousePressed(searchData);
 
 }
-
 
 
 
@@ -92,21 +83,22 @@ function loadKey(encrypted){
 
 function searchData(){
 
-	//Una serie de if para recorrer de forma naive los json para obtener las coincidencias, O(n) si tomamos nuestro n como la cantidad de peliculas tho
-	for (var movie of jsonMovies){
-		if (movie.getString("title") == title.value()){
-			jsonData.push(movie);
-		} else if (movie.getString("genre")	== genre.value()){
-			jsonData.push(movie);
-		}
-	}
+	var queryParams = {
+		"title": ui.TextBox["title"].value(),
+		"yearBegin" : parseInt(ui.TextBox["yearBegin"].value()),
+		"yearEnd" : parseInt(ui.TextBox["yearEnd"].value()),
+		"director": ui.TextBox["director"].value(),
+		"cast":  ui.TextBox["cast"].value(),
+		"genre": ui.TextBox["genre"].value()
+	};
 
-	showingData = [{"title":"After Dark in Central Park","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"Boarding School Girls' Pajama Parade","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"Buffalo Bill's Wild West Parad","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"Caught","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"Clowns Spinning Hats","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"Capture of Boer Battery by British","year":1900,"director":"James H. White","cast":null,"genre":"Short documentary","notes":null},{"title":"The Enchanted Drawing","year":1900,"director":"J. Stuart Blackton","cast":null,"genre":null,"notes":null},{"title":"Family Troubles","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"Feeding Sea Lions","year":1900,"director":null,"cast":"Paul Boyton","genre":null,"notes":null},{"title":"How to Make a Fat Wife Out of Two Lean Ones","year":1900,"director":null,"cast":null,"genre":"Comedy","notes":null},{"title":"New Life Rescue","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"New Morning Bath","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"Searching Ruins on Broadway, Galveston, for Dead Bodies","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"The Tribulations of an Amateur Photographer","year":1900,"director":null,"cast":null,"genre":null,"notes":null},{"title":"Trouble in Hogan's Alley","year":1900,"director":null,"cast":null,"genre":"Comedy","notes":null},{"title":"Two Old Sparks","year":1900,"director":null,"cast":null,"genre":"Short","notes":"Produced by Siegmund Lubin"},{"title":"The Wonder, Ching Ling Foo","year":1900,"director":null,"cast":"Ching Ling Foo","genre":"Short","notes":"Produced by Siegmund Lubin"},{"title":"Watermelon Contest","year":1900,"director":"James H. White","cast":null,"genre":"Short","notes":null}]
-	drawBubbles();
+	socket.emit('searchMovies', queryParams);
 }
 
-function drawBubbles(){
-	sortedMovies = orderByYears();
+function drawBubbles(movieResult){
+	console.log("Inicia");
+	console.log(movieResult);
+	sortedMovies = orderByYearsaAndCategory();
 }
 
 
@@ -114,6 +106,7 @@ function drawBubbles(){
 function orderByYearsaAndCategory(){
 	sortedMovies = {};
 
+/*
 	for (var movie of showingData){
 		if (!(movie.year in sortedMovies))					//Si el año de la pelicula no esta ingresado, se agrega
 			sortedMovies[movie.year] = {};
@@ -122,14 +115,10 @@ function orderByYearsaAndCategory(){
 
 		sortedMovies[movie.year][movie.gender].push(movie);
 	}
-
+*/
 	return sortedMovies;
 }
 
-
-//Se planeaba usar para comparar palabras que fueran similares
-function isSimilar(words, oration){
-}
 
 
 // Runs repeatedly until exit() is called.
